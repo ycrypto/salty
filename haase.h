acec3a685a67a3111ebddd8b324ffe0c9998f434
Nicolas Stalder <n@stalder.io>
2019-11-19 02:01:27 +0100

    Add C++ header file, and noisy warning about target

--- haase.h
+++ haase.h
@@ -1,4 +1,4 @@
 typedef unsigned int fe25519[8];
 
 void fe25519_mul_asm (fe25519 *pResult, const fe25519 *pVal1, const fe25519 *pVal2);
-void fe25519_sqr_asm (fe25519 *pResult, const fe25519 *pVal);
+void fe25519_square_asm (fe25519 *pResult, const fe25519 *pVal);
